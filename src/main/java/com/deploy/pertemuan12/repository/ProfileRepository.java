c2943e8fff5b3006d4fff8dd7ccbd8cd1762d6e0
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-05-18 19:01:50 +0700

    Change the typo code

--- src/main/java/com/deploy/pertemuan12/repository/ProfileRepository.java
+++ src/main/java/com/deploy/pertemuan12/repository/ProfileRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProfileRepository extends JpaRepository<Profile, String> {
-
+public interface ProfileRepository extends JpaRepository<Profile, String> {
 }
